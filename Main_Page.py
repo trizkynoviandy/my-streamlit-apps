17bea8be04013fe0704cffd695972db29bb0a77e
teukurizkyn <trizkynoviandy@gmail.com>
2022-06-06 22:55:52 +0700

    update code

--- Main_Page.py
+++ Main_Page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.markdown("# My Streamlit Apps")
-st.sidebar.markdown("# Main page 🎈")
+st.sidebar.markdown("# Main page")
 
 st.text("Collection of my apps deployed in streamlit")
--- pages/01_Classification.py
+++ pages/01_Classification.py
@@ -32,5 +32,49 @@ if option == "Movie Review Sentiment Prediction":
                 st.success('DONE: This is a positive review')
         except ValueError:
             st.write("Make sure your data is correct")
+            
+elif option == "Diabetes Prediction":
+    
+    loaded_model = joblib.load("models/classification/diabetes_prediction_model.sav")
+    st.markdown("""
+            # Diabetes Prediction Dashboard
+            * Predict whether or not you have diabetes, based on several medical predictor variable
+            
+            * The predictions obtained by this application have no medical value, to get accurate results please consult a doctor.
+            * Dataset source: <https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database>
+            """)
+    st.image("https://cdn.pixabay.com/photo/2014/11/12/19/25/diabetes-528678_960_720.jpg")
+    
+    st.write("1. Pregnancies")
+    user_input_1 = st.number_input("Number of times pregnant")
+    st.write("2. Glucose")
+    user_input_2 = st.number_input("Plasma glucose concentration a 2 hours in an oral glucose tolerance test")
+    st.write("3. Blood Pressure")
+    user_input_3 = st.number_input("Diastolic blood pressure (mm Hg)")
+    st.write("4. Skin Thickness")
+    user_input_4 = st.number_input("Triceps skin fold thickness (mm)")
+    st.write("5. Insulin")
+    user_input_5 = st.number_input("2-Hour serum insulin (mu U/ml)")
+    st.write("6. Body Mass Index")
+    user_input_6 = st.number_input("Body mass index (weight in kg/(height in m)^2)")
+    st.write("7. Diabetes Pedigree Function")
+    user_input_7 = st.number_input("Diabetes pedigree function")
+    st.write("8. Age")
+    user_input_8 = st.number_input("Age (years)")
+    input_data = [[user_input_1, user_input_2, user_input_3, user_input_4, user_input_5, user_input_6, user_input_7, user_input_8]]
+
+    predict = st.button("Predict")
+    
+    if predict:
+        try:
+            prediction = loaded_model.predict(input_data)
+            st.success('Prediction Succesful!')
+            if str(prediction) == "[0]":
+                st.write("Result: NEGATIVE")
+            else:
+                st.write("Result: POSITIVE")
+        except ValueError:
+            st.write("Make sure you have entered all the required data")
+    
 else:
     st.write('You selected:', option)
